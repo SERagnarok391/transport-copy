f3ecf6fe8f64a4d045bbfce4db97be6e9a184b76
James Irwin <rebolyte@gmail.com>
2018-06-12 16:58:15 -0500

    cleanup

--- src/js/main.ts
+++ src/js/main.ts
@@ -3,10 +3,10 @@ import { format } from 'date-fns';
 
 console.log('this is the JS', format(new Date(2014, 1, 11), 'MM/DD/YYYY'));
 
-var base = 'background-color: #575757; font: x-large serif; ';
-var desc = 'color: #888; font-style: italic;';
-var gray = 'color: #ddd';
-var dig = 'color: #111';
+let base = 'background-color: #575757; font: x-large serif; ';
+let desc = 'color: #888; font-style: italic;';
+let gray = 'color: #ddd';
+let dig = 'color: #111';
 
 console.log('%cSite constructed by %cGrayscale %cDigital', base + desc, base + gray, base + dig);
 console.log(
