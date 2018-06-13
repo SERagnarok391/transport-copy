9354d47ec54b9cb710c63bcd0ac73d653b6fd03c
James Irwin <rebolyte@gmail.com>
2018-06-12 19:58:44 -0500

    remove unnecessary deps

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,7 +1,4 @@
 import Vue from 'vue';
-import { format } from 'date-fns';
-
-console.log('this is the JS', format(new Date(2014, 1, 11), 'MM/DD/YYYY'));
 
 let base = 'background-color: #575757; font: x-large serif; ';
 let desc = 'color: #888; font-style: italic;';
